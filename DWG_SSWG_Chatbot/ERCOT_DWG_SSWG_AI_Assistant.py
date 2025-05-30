4c544a1c55a11ab444ccc8cb9323cca8a5408fcf
Amir Exir <amir.exir@utexas.edu>
2025-05-29 22:27:24 -0500

    """

--- DWG_SSWG_Chatbot/ERCOT_DWG_SSWG_AI_Assistant.py
+++ DWG_SSWG_Chatbot/ERCOT_DWG_SSWG_AI_Assistant.py
@@ -106,7 +106,7 @@ Only use the following documentation to answer the question:
 Instructions:
 - Stay factual and grounded strictly in the provided content.
 - If the answer is not explicitly found in the document, respond: "I couldn’t find that in the documentation."
-- Do NOT guess, assume, or rely on outside knowledge.
+- Do NOT guess, assume, or rely on outside knowledge."""
 }
 
         }
