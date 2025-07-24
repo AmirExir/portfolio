30a003664429e957c341338a053a2332f1da8cf3
Amir Exir <amir.exir@utexas.edu>
2025-07-24 14:13:11 -0500

    10000 large

--- chatbot_ercot/ercotAIassistant.py
+++ chatbot_ercot/ercotAIassistant.py
@@ -15,7 +15,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 @st.cache_data(show_spinner=False)
 def load_ercot_chunks_and_embeddings():
     from openai import OpenAI
-    embedding_model = "text-embedding-3-small"
+    embedding_model = "text-embedding-3-large"
 
     chunks = []
     embeddings = []
@@ -110,9 +110,9 @@ Instructions:
         messages = [system_prompt] + st.session_state.messages
 
         response = client.chat.completions.create(
-            model="gpt-4o-mini",
+            model="gpt-4o",
             messages=messages,
-            max_tokens=1024
+            max_tokens=10000
         )
 
         bot_msg = response.choices[0].message.content
