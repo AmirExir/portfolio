37f7e80c0d3209571ee5a35f4de9e21d003708b1
AmirExir <amir.exir@utexas.edu>
2025-08-09 23:49:00 -0500

    Revert "Reapply "reqs""
    This reverts commit 3d471befde7e19aa5458fb38616b39f6133c973b.

--- chatbot_ercot_nodalprotocols/ercotProtocolsAssistant.py
+++ chatbot_ercot_nodalprotocols/ercotProtocolsAssistant.py
@@ -7,8 +7,6 @@ from openai import OpenAI
 from sklearn.metrics.pairwise import cosine_similarity
 from typing import List
 import numpy as np
-import hashlib
-import time
 
 # Set up OpenAI client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
